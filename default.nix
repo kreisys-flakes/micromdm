4c5fcfaa3db82d5e1ed90b43281660cec5cbf75a
Shay Bergmann <shaybergmann@gmail.com>
2021-06-22 08:04:25 -0400

    init

---
+++ default.nix
@@ -0,0 +1,18 @@
+{ lib, buildGoModule, fetchFromGitHub, src, version }:
+
+buildGoModule rec {
+  pname = "micromdm";
+
+  inherit src version;
+
+  vendorSha256 = "sha256-voshHQjUAK13rfjEyH8ClpBBmV+vBBGnphn5Z1gUVjE=";
+
+  runVend = true;
+
+  meta = with lib; {
+    description = "Mobile Device Management server";
+    homepage = "https://micromdm.io";
+    license = licenses.mit;
+    platforms = platforms.linux ++ platforms.darwin;
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Mobile Device Management server";
+
+  inputs = {
+    utils.url = "github:kreisys/flake-utils";
+    src.url = "github:micromdm/micromdm";
+    src.flake = false;
+  };
+
+  outputs = { self, utils, nixpkgs, src }: utils.lib.simpleFlake {
+    inherit nixpkgs;
+    systems = [ "x86_64-linux" "x86_64-darwin" ];
+
+    overlay = final: prev: {
+      micromdm = final.callPackage self { inherit src; version = src.shortRev; };
+    };
+
+    packages = { micromdm }: {
+      inherit micromdm;
+      defaultPackage = micromdm;
+    };
+  };
+}
